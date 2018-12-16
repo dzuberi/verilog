3690ccdb30a20502bb4c5d4e1a4e950c934cfc19
Danial Zuberi <dzuberi522@gmail.com>
2018-12-16 03:05:27 -0500

    Multiplier working

--- mul.v
+++ mul.v
@@ -1,18 +1,30 @@
-module mul#(parameter DATA_WID = 4)(a,b,enable,out,overflow);
+module mul#(parameter DATA_WID = 4)(a,b,enable,outlo, outhi,overflow);
 	input [DATA_WID - 1:0] a, b;
 	input enable;
-	output[DATA_WID*2 - 1:0] out;
+	wire [DATA_WID*2 - 1:0] out;
+	output [DATA_WID - 1 : 0] outlo;
+	output [DATA_WID - 1 : 0] outhi;
 	output overflow;
-	wire [DATA_WID - 1 : 0] partial_sums [DATA_WID - 1 : 0];
+	wire [DATA_WID - 1 : 0] partial_muls [DATA_WID - 1 : 0];
 	genvar i,j;
 	//generate
 	for (i = 0; i < DATA_WID; i = i + 1) begin
-		for (j = 0; i < DATA_WID; i = i + 1) begin
-			assign partial_sums[i][j] = a[i] | b[j];
-		end
+		assign partial_muls[i] = {DATA_WID{a[i]}} & b;
 	end
 	//endgenerate
-
-
-
+	wire [DATA_WID*2 - 1 : 0] shifted_muls [DATA_WID - 1 : 0];
+	for (i = 0; i < DATA_WID; i = i + 1) begin
+		assign shifted_muls[i] = partial_muls[i] << i;
+	end
+	wire [DATA_WID*2 - 1 : 0] partial_sums [DATA_WID - 2 : 0];
+	wire[DATA_WID - 2 : 0] unused ;
+	generate
+	adder #(DATA_WID*2) add_part0 (shifted_muls[0],shifted_muls[1], enable, partial_sums[0], unused[0]);
+	for (i = 1; i < DATA_WID - 1; i = i + 1) begin : generate_block_identifier
+		adder #(DATA_WID*2) add_part (partial_sums[i - 1],shifted_muls[i+1],enable,partial_sums[i],unused[i]);
+	end
+	endgenerate
+	assign out = partial_sums [DATA_WID - 2];
+	assign outlo = out[DATA_WID - 1 : 0];
+	assign outhi = out[DATA_WID*2 - 1 : DATA_WID];
 endmodule // adder
--- mul_tb.v
+++ mul_tb.v
@@ -1,26 +1,30 @@
 `timescale 1ns/1ps
 module mul_tb();
-	//parameter DATA_WID = 2;
-	reg[3:0] a,b;
-	wire[7:0] out;
+	parameter DATA_WID = 8;
+	reg[DATA_WID - 1:0] a,b;
+	wire[DATA_WID - 1:0] outlo;
+	wire [DATA_WID - 1:0] outhi;
 	reg enable;
 	wire overflow;
 
 	initial begin
-		$dumpvars(0,mul_tb);
-		a <= 4'd0;
-		b <= 4'd0;
+		$dumpvars(0,DUT);
+		a <= {{(DATA_WID - 4){1'b0}}, 4'd0};
+		b <= {{(DATA_WID - 4){1'b0}}, 4'd0};
 		enable = 0;
 		#20
 		enable = 1;
 		#20
-		a <= 4'd2;
-		b <= 4'd4;
+		a <= {{(DATA_WID - 4){1'b0}}, 4'd2};
+		b <= {{(DATA_WID - 4){1'b0}}, 4'd4};
 		#20
-		a <= 4'd1;
-		b <= 4'd10;
+		a <= {{(DATA_WID - 4){1'b0}}, 4'd1};
+		b <= {{(DATA_WID - 4){1'b0}}, 4'd10};
 		#20
-		$stop;
+		a <= {{(DATA_WID - 4){1'b1}}, 4'd12};
+		b <= {{(DATA_WID - 4){1'b1}}, 4'd12};
+		#20
+		$finish;
 	end
-	mul#(4) DUT(.a(a),.b(b),.enable(enable),.out(out),.overflow(overflow));
+	mul#(DATA_WID) DUT(.a(a),.b(b),.enable(enable),.outlo(outlo), .outhi(outhi),.overflow(overflow));
 endmodule
